649599ed562756885ee27ff60e7f25079271f804
yamayosi <vanha.muist@gmail.com>
2015-06-17 06:07:17 +0900

    Commit Test

--- app/src/main/java/jushin/net/homepagebuilder/JavaClassForS.java
+++ app/src/main/java/jushin/net/homepagebuilder/JavaClassForS.java
@@ -4,4 +4,7 @@ package jushin.net.homepagebuilder;
  * Created by yulth on 15/06/16.
  */
 public class JavaClassForS {
+    public static void main(STring[] args){
+        System.out.println("I'm so sleepy!");
+    }
 }
