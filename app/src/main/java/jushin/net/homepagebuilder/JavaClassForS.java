c7450200103b55fc38709681110fa71e377143e9
yutayuta3560 <yulth1123@gmail.com>
2015-06-17 07:17:04 +0900

    test

--- app/src/main/java/jushin/net/homepagebuilder/JavaClassForS.java
+++ app/src/main/java/jushin/net/homepagebuilder/JavaClassForS.java
@@ -1,10 +1,18 @@
 package jushin.net.homepagebuilder;
 
+import java.lang.System;
+
 /**
  * Created by yulth on 15/06/16.
  */
 public class JavaClassForS {
-    public static void main(STring[] args){
+    public static void main(String[] args){
         System.out.println("No reply");
+
     }
+
+    public static void main2() {
+        System.out.println("やっほー");
+    }
+
 }
