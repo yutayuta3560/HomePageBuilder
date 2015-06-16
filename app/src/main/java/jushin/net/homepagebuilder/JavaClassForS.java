21e54a24084be414a6f27704cb7acf1e2b321bf1
yutayuta3560 <yulth1123@gmail.com>
2015-06-17 07:37:17 +0900

    (っ・ワ・)

--- app/src/main/java/jushin/net/homepagebuilder/JavaClassForS.java
+++ app/src/main/java/jushin/net/homepagebuilder/JavaClassForS.java
@@ -6,13 +6,13 @@ import java.lang.System;
  * Created by yulth on 15/06/16.
  */
 public class JavaClassForS {
+
     public static void main(String[] args){
         System.out.println("No reply");
-
     }
 
     public static void main2() {
-        System.out.println("よわっち()");
+        System.out.println("よわっち(っ・ワ・)");
     }
 
 }
